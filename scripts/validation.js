0c2c3ad0714a229390f868735444f204443915ce
Vitor Lourenco <vitor.registros@lourencos.net>
2021-04-20 15:26:17 -0300

    Change URL validation regexp

--- scripts/validation.js
+++ scripts/validation.js
@@ -17,7 +17,8 @@ function validate(callback, strValidationType, strSource){
 
 function factoryURL(str){
   if (typeof(str)!=='string') return makeValidationObj(false, 'invalid input source');
-  if (!(/^(http:\/\/)/.test(str) || /^(https:\/\/)/.test(str))) return makeValidationObj(false, 'URL da imagem deve comecar com http:// ou https://');
+  const urlRegexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (!urlRegexp.test(str)) return makeValidationObj(false, 'URL da imagem deve ser valida');
   return makeValidationObj(true, str);
 }
 
