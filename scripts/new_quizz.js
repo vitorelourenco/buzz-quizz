2c5f4c11b22257c01321f1a2679decf838e6d886
Vitor Lourenco <vitor.registros@lourencos.net>
2021-04-20 16:52:00 -0300

    Fix indentation from autosave

--- scripts/new_quizz.js
+++ scripts/new_quizz.js
@@ -17,8 +17,8 @@ newQuizzObj = {
                     isCorrectAnswer: false
                 }
             ]
-        },
-        {
+            },
+            {
             title: "Título da pergunta 2",
             color: "#123456",
             answers: [{
@@ -32,8 +32,8 @@ newQuizzObj = {
                     isCorrectAnswer: false
                 }
             ]
-        },
-        {
+            },
+            {
             title: "Título da pergunta 3",
             color: "#123456",
             answers: [{
@@ -54,8 +54,8 @@ newQuizzObj = {
             image: "https://http.cat/411.jpg",
             text: "Descrição do nível 1",
             minValue: 0
-        },
-        {
+            },
+            {
             title: "Título do nível 2",
             image: "https://http.cat/412.jpg",
             text: "Descrição do nível 2",
@@ -81,11 +81,6 @@ function selectUnique(domElem) {
     console.log(currentNode);
     currentNode.classList.remove('collapsed');
 }
-
-// function toggleCollapsed(elem){
-//   const parent = elem.parentNode;
-//   parent.classList.toggle('collapsed');
-// }
 
 function handleStartSubmit() {
     objNewStart = document.querySelector('.new-quizz');
@@ -160,58 +155,56 @@ function handleLevelsSubmit() {
 
 
 function buildNewQuizzPageStart() {
-    //replace test with the approptiate div later
-    const container = document.querySelector('.test');
+    const container = document.querySelector('.page-container');
     container.innerHTML =
-        `
-  <section class="new-quizz">
-    <h2>Comece pelo comeco</h2>
-    <div class="input-group padding-20">
-        <input class="new-quizz-title" type="text" placeholder="Titulo do seu quizz">
-        <input class="new-quizz-image" type="text" placeholder="URL da imagem do seu quizz">
-        <input class="new-quizz-nQuestions" type="text" placeholder="Quantidade de perguntas do quizz">
-        <input class="new-quizz-nLevels" type="text" placeholder="Quantidade de niveis do quizz">
-    </div>
-    <button onclick='handleStartSubmit()'>Prosseguir para criar perguntas</button>
-  </section>
-  `
+    `
+    <section class="new-quizz">
+      <h2>Comece pelo comeco</h2>
+      <div class="input-group padding-20">
+          <input class="new-quizz-title" type="text" placeholder="Titulo do seu quizz">
+          <input class="new-quizz-image" type="text" placeholder="URL da imagem do seu quizz">
+          <input class="new-quizz-nQuestions" type="text" placeholder="Quantidade de perguntas do quizz">
+          <input class="new-quizz-nLevels" type="text" placeholder="Quantidade de niveis do quizz">
+      </div>
+      <button onclick='handleStartSubmit()'>Prosseguir para criar perguntas</button>
+    </section>
+    `;
 }
 
 function buildNewQuizzPageQuestions() {
-    //replace test with the approptiate div later
-    const container = document.querySelector('.test');
+    const container = document.querySelector('.page-container');
     container.innerHTML =
-        `
-  <section class="new-quizz">
-    <h2>Crie suas perguntas</h2>
-  </section>
-  `;
+    `
+    <section class="new-quizz">
+      <h2>Crie suas perguntas</h2>
+    </section>
+    `;
 
     const section = container.querySelector('SECTION');
     for (let i = 0; i < newQuizzObj.questions.length; i++) {
         section.innerHTML +=
-            `
-    <div class="input-group collapsed">
-      <header onclick='selectUnique(this)'>
-        <h3>Pergunta ${i+1}</h3>
-        <img class="svg" src="assets/images/edit.svg">
-      </header>
-      <div class="collapsible">
-        <input class="question-title" type="text" placeholder="Texto da pergunta">
-        <input class="question-background" type="text" placeholder="Cor de fundo da pergunta">
-        <h3>Resposta correta</h3>
-        <input class="question-answer" type="text" placeholder="Resposta correta">
-        <input class="question-image" type="text" placeholder="URL da imagem">
-        <h3>Respostas incorretas</h3>
-        <input class="question-answer" type="text" placeholder="Resposta incorreta 1">
-        <input class="question-image" type="text" placeholder="URL da imagem 1">
-        <input class="question-answer" type="text" placeholder="Resposta incorreta 2">
-        <input class="question-image" type="text" placeholder="URL da imagem 2">
-        <input class="question-answer" type="text" placeholder="Resposta incorreta 3">
-        <input class="question-image" type="text" placeholder="URL da imagem 3">
-      </div>
-    </div>
-    `
+        `
+        <div class="input-group collapsed">
+          <header onclick='selectUnique(this)'>
+            <h3>Pergunta ${i+1}</h3>
+            <img class="svg" src="assets/images/edit.svg">
+          </header>
+          <div class="collapsible">
+            <input class="question-title" type="text" placeholder="Texto da pergunta">
+            <input class="question-background" type="text" placeholder="Cor de fundo da pergunta">
+            <h3>Resposta correta</h3>
+            <input class="question-answer" type="text" placeholder="Resposta correta">
+            <input class="question-image" type="text" placeholder="URL da imagem">
+            <h3>Respostas incorretas</h3>
+            <input class="question-answer" type="text" placeholder="Resposta incorreta 1">
+            <input class="question-image" type="text" placeholder="URL da imagem 1">
+            <input class="question-answer" type="text" placeholder="Resposta incorreta 2">
+            <input class="question-image" type="text" placeholder="URL da imagem 2">
+            <input class="question-answer" type="text" placeholder="Resposta incorreta 3">
+            <input class="question-image" type="text" placeholder="URL da imagem 3">
+          </div>
+        </div>
+        `
     }
 
     section.innerHTML += `<button onclick='handleQuestionsSubmit()'>Prosseguir para criar niveis</button>`
@@ -222,33 +215,32 @@ function buildNewQuizzPageQuestions() {
 }
 
 function buildNewQuizzPageLevels() {
-    //replace test with the approptiate div later
-    const container = document.querySelector('.test');
+    const container = document.querySelector('.page-container');
     container.innerHTML =
-        `
-  <section class="new-quizz">
-    <h2>Agora, decida os niveis!</h2>
-  </section>
-  `;
+    `
+    <section class="new-quizz">
+      <h2>Agora, decida os niveis!</h2>
+    </section>
+    `;
 
     const section = container.querySelector('SECTION');
     // for (let i=0; i<newQuizzObj.questions.length; i++){
     for (let i = 0; i < 2; i++) {
         section.innerHTML +=
-            `
-      <div class="input-group collapsed">
-        <header onclick='selectUnique(this)'>
-          <h3>Nivel ${i+1}</h3>
-          <img class="svg" src="assets/images/edit.svg">
-        </header>
-        <div class="collapsible">
-          <input class="level-title" type="text" placeholder="Titulo do nivel">
-          <input class="level-percentage" type="text" placeholder="% de acerto minima">
-          <input class="level-image" type="text" placeholder="URL da imagem do nivel">
-          <textarea class="level-description" type="text" placeholder="Descricao do nivel"></textarea>
+        `
+        <div class="input-group collapsed">
+          <header onclick='selectUnique(this)'>
+            <h3>Nivel ${i+1}</h3>
+            <img class="svg" src="assets/images/edit.svg">
+          </header>
+          <div class="collapsible">
+            <input class="level-title" type="text" placeholder="Titulo do nivel">
+            <input class="level-percentage" type="text" placeholder="% de acerto minima">
+            <input class="level-image" type="text" placeholder="URL da imagem do nivel">
+            <textarea class="level-description" type="text" placeholder="Descricao do nivel"></textarea>
+          </div>
         </div>
-      </div>
-    `
+        `
     }
 
     section.innerHTML += `<button onclick='handleLevelsSubmit()'>Finalizar Quizz</button>`
@@ -266,28 +258,26 @@ function buildNewQuizzPageDone(id) {
         .then(({ data }) => {
             imgSrc = data.image;
             title = data.title;
-            //replace test with the approptiate div later
-            const container = document.querySelector('.test');
+            const container = document.querySelector('.page-container');
             container.innerHTML =
-                `
-    <section class="new-quizz">
-      <h2>Seu quizz esta pronto!</h2>
-      <figure>
-        <img src="${imgSrc}" alt=${title}>
-        <figcaption>${title}</figcaption>
-      </figure>
-      <button class="go-to-quizz" onclick='goToQuizz(${id})'>Acessar Quizz</button>
-      <button class="back-to-home" onclick='goToHome()'>Voltar para home</button>
-    </section>
-    `;
+            `
+            <section class="new-quizz">
+              <h2>Seu quizz esta pronto!</h2>
+              <figure>
+                <img src="${imgSrc}" alt=${title}>
+                <figcaption>${title}</figcaption>
+              </figure>
+              <button class="go-to-quizz" onclick='goToQuizz(${id})'>Acessar Quizz</button>
+              <button class="back-to-home" onclick='goToHome()'>Voltar para home</button>
+            </section>
+            `;
         })
         .catch((error) => {
             alert(`error ${error.response.status}`);
         });
-
 };
 
-// buildNewQuizzPageDone(1);
+buildNewQuizzPageDone(1);
 // buildNewQuizzPageStart();
 // buildNewQuizzPageLevels();
 // buildNewQuizzPageQuestions({ title: 1, image: 1, questions: [1, 2, 3], levels: [1, 2, 3] })
