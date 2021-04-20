55cbaa2157d7f0502ad5d687a8527773e805f1c5
Vitor Lourenco <vitor.registros@lourencos.net>
2021-04-19 21:43:28 -0300

    Make css and JS function to build new-quizz-start

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
-console.log('taindo');
+//replace body with something else later
+function buildNewQuizzPageStart(){
+  document.querySelector('BODY').innerHTML =
+  `
+  <section class="new-quizz">
+    <h2>Comece pelo comeco</h2>
+    <div class="input-group">
+        <input type="text" placeholder="Titulo do seu quizz">
+        <input type="text" placeholder="URL da imagem do seu quizz">
+        <input type="text" placeholder="Quantidade de perguntas do quizz">
+        <input type="text" placeholder="Quantidade de niveis do quizz">
+    </div>
+    <button>Prosseguir para criar perguntas</button>
+  </section>
+  `
+}
+
+buildNewQuizzPageStart();
