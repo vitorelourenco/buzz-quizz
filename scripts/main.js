d4c376371236807ba650d7da1ec5d48d2690a9dd
Victor Mayer <victor.mayer@outlook.com>
2021-04-23 13:11:56 -0300

    label done

--- scripts/main.js
+++ scripts/main.js
@@ -72,8 +72,10 @@ function buildQuizzList(resposta, arrLocalIds) {
                                 <li onclick="getQuizz(${quizzList[i].id})" class="quizz-thumb">
                                     <div></div>
                                     <span class="label">
-                                        <img src="assets/images/edit-white.svg">
-                                        <ion-icon name="trash"></ion-icon>
+                                        <span class="edit">
+                                            <img src="assets/images/edit-white.svg">
+                                        </span>
+                                        <ion-icon name="trash-outline"></ion-icon>
                                     </span>
                                     <img src=${quizzList[i].image}" alt="quizz thumbnail">
                                     <p>${quizzList[i].title}</p>
